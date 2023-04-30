5f3148b9d9bdd2c0e5e87fc25a18afd23d67bb6a
eno-conan <eno49study@gmail.com>
2023-05-01 06:45:14 +0900

    first commit

---
+++ app/root.tsx
@@ -0,0 +1,45 @@
+import { cssBundleHref } from "@remix-run/css-bundle";
+import type { LinksFunction, LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+import { getUser } from "~/session.server";
+import tailwindStylesheetUrl from "~/styles/tailwind.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: tailwindStylesheetUrl },
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+];
+
+export const loader = async ({ request }: LoaderArgs) => {
+  return json({ user: await getUser(request) });
+};
+
+export default function App() {
+  return (
+    <html lang="en" className="h-full">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body className="h-full">
+        <Outlet />
+        <ScrollRestoration />
+        {/* https://remix.run/docs/en/main/components/scroll-restoration */}
+        <Scripts />
+        {/* https://remix.run/docs/en/main/components/scripts */}
+        <LiveReload />
+        {/* https://remix.run/docs/en/main/components/live-reload */}
+      </body>
+    </html>
+  );
+}
