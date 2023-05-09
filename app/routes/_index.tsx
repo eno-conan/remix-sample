4b79c580a451c288a143100eb7ca1caba7026046
eno-conan <eno49study@gmail.com>
2023-05-09 09:39:15 +0900

    トップ画面での画像取得処理実装

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import { json, LoaderArgs, V2_MetaFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
+import { createClient } from "@supabase/supabase-js";
 import { useState } from "react";
 
 export const meta: V2_MetaFunction = () => [{ title: "Remix Notes" }];
@@ -7,17 +8,41 @@ export const meta: V2_MetaFunction = () => [{ title: "Remix Notes" }];
 const samplePicturesArr = ['1', '2', '3', '4', '5', '6']
 
 export const loader = async ({ request }: LoaderArgs) => {
-  return json({
-    ENV: {
-      GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
-    },
-  });
+  const supabaseClient = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!)
+  const { data, error } = await supabaseClient
+    .storage
+    .from('place-images')
+    .list('image', {
+      limit: 3,
+      offset: 0,
+      sortBy: { column: 'name', order: 'asc' },
+    })
+  // 画像ファイル名を格納し、画面表示用のURLを取得
+  const names: string[] = []
+  data!.forEach((p: any) => {
+    // .emptyFolderPlaceholderの場合は表示しない
+    if (p.name !== '.emptyFolderPlaceholder') {
+      names.push(`image/${p.name}`);
+    }
+  })
+  if (names.length > 0) {
+    const signedUrls = await supabaseClient
+      .storage
+      .from('place-images')
+      .createSignedUrls(names, 60)
+    return json({
+      signedUrls: signedUrls.data
+    });
+  } else {
+    return json({
+      signedUrls: []
+    });
+  }
 };
-
-
 
 export default function Index() {
   const [isLoading, setIsLoading] = useState(false);
+  const data = useLoaderData<typeof loader>();
 
   const handleClick = () => {
     setIsLoading(true);
@@ -29,12 +54,12 @@ export default function Index() {
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">最近の写真</div>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-            {samplePicturesArr.map((p) => (
-              <div className="col-span-1" key={p}>
+            {data.signedUrls!.length > 0 && data.signedUrls!.map((p) => (
+              <div className="col-span-1" key={p.signedUrl}>
                 <div className="bg-white rounded-lg overflow-hidden shadow-md">
-                  <img className="w-full" src="https://via.placeholder.com/640x360.png" alt={p} />
+                  <img className="w-full" src={p.signedUrl} alt={p.signedUrl} />
                   <div className="p-4">
-                    <h3 className="font-medium text-lg">{p}</h3>
+                    <h3 className="font-medium text-lg">Text</h3>
                     <p className="text-gray-500 mt-2">Card description</p>
                   </div>
                 </div>
