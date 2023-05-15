0b6275dbb2c49b69af46e3bf012027de8b42ca0b
eno-conan <eno49study@gmail.com>
2023-05-15 10:10:45 +0900

    ジャンルとエリアのプルダウンを追加して、その内容に応じたおすすめの場所が表示される状態に修正 （コストセーブのため、ダミーの回答を表示する状態にしてある）

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,7 +2,7 @@ import { Link } from "@remix-run/react";
 
 const Header = () => {
     return (
-        <header className="border-b items-center h-16 bg-green-200">
+        <header className="border-b items-center h-20 bg-green-200">
             <h1 className="text-4xl text-green-500 font-semibold py-2 px-8">
                 <Link to="/">
                     <span className="">Visit History Site</span>
@@ -10,21 +10,21 @@ const Header = () => {
             </h1>
             <div className="flex justify-between w-full py-4 px-8 shadow-md bg-gray-200">
                 <Link to="/top" className="inline-block px-4 py-2 text-green-500 text-lg font-semibold hover:underline">
-                    最近を振り返る
+                    直近を振り返る
                 </Link>
-                . <Link to="/list" className="inline-block px-4 py-2 text-green-500 text-lg font-semibold hover:underline">
-                    これまでの写真を確認する
+                <Link to="/list" className="inline-block px-4 py-2 text-green-500 text-lg font-semibold hover:underline">
+                    過去の写真を確認する
                 </Link>
-                . <Link to="/add" className="inline-block px-4 py-2 text-green-500 text-lg font-semibold hover:underline">
-                    新しく記録を追加する
+                <Link to="/add" className="inline-block px-4 py-2 text-green-500 text-lg font-semibold hover:underline">
+                    記録を追加する
                 </Link>
-                . <Link to="/search" className="inline-block px-4 py-2 text-green-500 text-lg font-semibold hover:underline">
+                <Link to="/search" className="inline-block px-4 py-2 text-green-500 text-lg font-semibold hover:underline">
                     候補地を探す
                 </Link>
-                . <Link to="." className="inline-block px-4 py-2 text-green-500 text-lg font-semibold hover:underline">
+                <Link to="." className="inline-block px-4 py-2 text-green-500 text-lg font-semibold hover:underline">
                     ？？？
                 </Link>
-                . <Link to="." className="inline-block px-4 py-2 text-green-500 text-lg font-semibold hover:underline">
+                <Link to="." className="inline-block px-4 py-2 text-green-500 text-lg font-semibold hover:underline">
                     設定を変更する
                 </Link>
             </div>
--- app/routes/search.tsx
+++ app/routes/search.tsx
@@ -1,7 +1,8 @@
 import { json, LoaderArgs } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
-import { Configuration, OpenAIApi } from "openai";
+import { useLoaderData } from "@remix-run/react";
+import React from "react";
 import { useEffect, useState } from "react";
+import { BeatLoader, PropagateLoader } from "react-spinners";
 import { getRecommendPlace } from "~/hooks/chatgpt"
 
 // 画面表示時にGoogle API Keyの値取得
@@ -13,43 +14,174 @@ export const loader = async ({ request }: LoaderArgs) => {
     });
 };
 
+const junre = [
+    { label: '選択してください', value: '' },
+    { label: '海・河川', value: '海・河川' },
+    { label: '山・森林', value: '山・森林' },
+    { label: 'アクティビティ', value: 'アクティビティ' },
+    { label: '歴史的建造物', value: '歴史的建造物' },
+];
+
+const aria = [
+    { label: '選択してください', value: '' },
+    { label: '北海道', value: '北海道' },
+    { label: '東北', value: '東北' },
+    { label: '関東', value: '関東' },
+    { label: '近畿', value: '近畿' },
+]
 
 export default function Search() {
     const data = useLoaderData<typeof loader>();
     const [chatGptAns, setChatGptAns] = useState<string>("");
+    const [isLoading, setIsLoading] = useState(false);
+
+    // プルダウン
+    const [selectedA, setSelectedA] = useState('');
+    const [selectedB, setSelectedB] = useState('');
+    const handleSelectA = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedA(event.target.value);
+    };
+    const handleSelectB = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedB(event.target.value);
+    };
 
     async function searchRecommendationPlaces() {
-        // getRecommendPlace(data.ENV.OPENAI_API_KEY ?? '');
-        const configuration = new Configuration({
-            apiKey: data.ENV.OPENAI_API_KEY,
-        });
-        delete configuration.baseOptions.headers['User-Agent'];
-        const content = "進撃の巨人の「リヴァイ」について30文字で教えてください。";
-        const openai = new OpenAIApi(configuration);
-        const response = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages: [{ role: "user", content: content }],
-        });
+        // バリデーション
+        if (selectedA.length === 0 || selectedB.length === 0) {
+            alert("プルダウンから値を選択してください");
+            return
+        }
+        setIsLoading(true);
+        const questionContent = `${selectedA}の${selectedB}で良い写真が取れそうな場所を2つ教えてください。また各場所について知ることができるリンクも合わせてください。`;
+        try {
+            // Dummy Data
+            await sleep(1000);
+            setChatGptAns('abc');
+            // const ans = await getRecommendPlace(data.ENV.OPENAI_API_KEY ?? '', questionContent);
+            // setChatGptAns(ans ?? '');
+        } finally {
+            setIsLoading(false);
+        }
+    }
 
-        // レスポンスとしては30秒くらいかな？
-        const answer = response.data.choices[0].message?.content;
-        setChatGptAns(answer ?? '');
+    const BeatLoaderAria = () => {
+        return (
+            <div className="flex justify-center items-center pt-4">
+                <BeatLoader color="#36d7b7" size={18} />
+                {/* <PropagateLoader color="#36d7b7" size={20} /> */}
+            </div>
+        )
     }
 
     return (
         <>
             <main className="bg-gray-100 sm:items-center sm:justify-center mt-20 pb-10">
-                <div>
-                    <div className="text-center mb-4">
-                        <div className="w-1/4 text-xl bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={() => searchRecommendationPlaces()}>聞いてみる（リヴァイって誰？）</div>
+                <div className="flex justify-center items-center py-4">
+                    <div className="w-1/2 px-10">
+                        <label htmlFor="select-a" className="block text-gray-700 font-bold my-2">
+                            ジャンル
+                        </label>
+                        <div className="relative">
+                            <select
+                                id="select-a"
+                                value={selectedA}
+                                onChange={handleSelectA}
+                                className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                            >
+                                {junre.map((sct) => (
+                                    <option value={sct.value} key={sct.value}>{sct.label}</option>
+                                ))}
+                            </select>
+                            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                                <svg
+                                    className="fill-current h-4 w-4"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M6.293 7.293a1 1 0 011.414 0L10 9.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </div>
+                        </div>
                     </div>
-                    <h3>回答結果</h3>
-                    {chatGptAns ?
-                        <>{chatGptAns}</> :
-                        <>Loading...</>}
+                    <div className="w-1/2 px-10">
+                        <label htmlFor="select-b" className="block text-gray-700 font-bold my-2">
+                            エリア
+                        </label>
+                        <div className="relative">
+                            <select
+                                id="select-b"
+                                value={selectedB}
+                                onChange={handleSelectB}
+                                className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                            >
+                                {aria.map((sct) => (
+                                    <option value={sct.value} key={sct.value}>{sct.label}</option>
+                                ))}
+                            </select>
+                            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                                <svg
+                                    className="fill-current h-4 w-4"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M6.293 7.293a1 1 0 011.414 0L10 9.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="text-center my-4">
+                        <button
+                            className="w-1/6 text-xl bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded disabled:bg-gray-700"
+                            onClick={() => searchRecommendationPlaces()}
+                            disabled={isLoading}>
+                            聞いてみる</button>
+                    </div>
+                    <div className="pl-8 text-xl">以下に回答を表示します（～20秒かかります）</div>
+                    {isLoading ?
+                        <>
+                            <BeatLoaderAria />
+                        </>
+                        :
+                        <>
+                            <div className="px-6 font-semibold pt-4">
+                                {chatGptAns.split("\n").map((line, index) => (
+                                    <React.Fragment key={index}>
+                                        {line}
+                                        <br />
+                                    </React.Fragment>
+                                ))}
+                                {chatGptAns ?
+                                    <>
+                                        <div className="mt-4">
+                                            <button
+                                                className="w-1/6 text-xl disabled:bg-gray-700 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                                                // onClick={() => searchRecommendationPlaces()}
+                                                disabled={true}>
+                                                追加で質問</button>
+                                        </div>
+                                    </>
+                                    :
+                                    <></>}
+                            </div>
+                        </>
+                    }
                 </div>
             </main>
         </>
-
     )
+}
+
+// ちょっとスリープを設けたいときに使用する
+async function sleep(ms: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
